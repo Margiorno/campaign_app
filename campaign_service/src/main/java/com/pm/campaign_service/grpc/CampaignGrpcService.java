2277ee341d95c9f9c8737fa770571741b8c92b5f
Margiorno <m.zawiski03@gmail.com>
2025-07-12 22:01:56 +0200

    campaign grpc service

---
+++ campaign_service/src/main/java/com/pm/campaign_service/grpc/CampaignGrpcService.java
@@ -0,0 +1,63 @@
+package com.pm.campaign_service.grpc;
+
+import com.pm.campaign_service.mapper.CampaignMapper;
+import com.pm.proto.CampaignProto;
+import com.pm.proto.CampaignServiceGrpc;
+import com.pm.campaign_service.service.CampaignService;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
+
+@GrpcService
+public class CampaignGrpcService extends CampaignServiceGrpc.CampaignServiceImplBase {
+
+    private final CampaignService campaignService;
+
+    @Autowired
+    public CampaignGrpcService(CampaignService campaignService) {
+        this.campaignService = campaignService;
+    }
+
+    @Override
+    public void getCampaignById(CampaignProto.CampaignRequest request, StreamObserver<CampaignProto.CampaignResponse> responseObserver) {
+        var id = UUID.fromString(request.getId());
+        var responseDTO = campaignService.findById(id);
+        var response = CampaignMapper.toProto(responseDTO);
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void createCampaign(CampaignProto.CampaignRequest request, StreamObserver<CampaignProto.CampaignResponse> responseObserver) {
+        var requestDTO = CampaignMapper.fromProto(request);
+        var responseDTO = campaignService.save(requestDTO);
+        var response = CampaignMapper.toProto(responseDTO);
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void updateCampaign(CampaignProto.CampaignRequest request, StreamObserver<CampaignProto.CampaignResponse> responseObserver) {
+        var id = UUID.fromString(request.getId());
+        var requestDTO = CampaignMapper.fromProto(request);
+        var responseDTO = campaignService.update(requestDTO, id);
+        var response = CampaignMapper.toProto(responseDTO);
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void deleteCampaign(CampaignProto.CampaignRequest request, StreamObserver<CampaignProto.CampaignResponse> responseObserver) {
+        var id = UUID.fromString(request.getId());
+        campaignService.delete(id);
+
+        var response = CampaignProto.CampaignResponse.newBuilder().setId(id.toString()).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
--- campaign_service/src/main/java/com/pm/campaign_service/mapper/CampaignMapper.java
+++ campaign_service/src/main/java/com/pm/campaign_service/mapper/CampaignMapper.java
@@ -5,6 +5,7 @@ import com.pm.campaign_service.dto.CampaignResponseDTO;
 import com.pm.campaign_service.model.Campaign;
 import com.pm.campaign_service.model.City;
 import com.pm.campaign_service.model.Product;
+import com.pm.proto.CampaignProto;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,5 +45,33 @@ public class CampaignMapper {
         campaign.setRadius(Double.parseDouble(campaignRequestDTO.getRadius()));
 
         return campaign;
+    }
+
+    public static CampaignProto.CampaignResponse toProto(CampaignResponseDTO dto) {
+        return CampaignProto.CampaignResponse.newBuilder()
+                .setId(dto.getId())
+                .setName(dto.getName())
+                .setDescription(dto.getDescription())
+                .setProduct(dto.getProduct())
+                .addAllKeywords(dto.getKeywords())
+                .setBidAmount(Double.parseDouble(dto.getBid_amount()))
+                .setCampaignAmount(Double.parseDouble(dto.getCampaign_amount()))
+                .setActive(Boolean.parseBoolean(dto.getActive()))
+                .setCity(dto.getCity())
+                .setRadius(Double.parseDouble(dto.getRadius()))
+                .build();
+    }
+
+    public static CampaignRequestDTO fromProto(CampaignProto.CampaignRequest request) {
+        CampaignRequestDTO dto = new CampaignRequestDTO();
+        dto.setName(request.getName());
+        dto.setDescription(request.getDescription());
+        dto.setProduct(request.getProduct());
+        dto.setKeywords(request.getKeywordsList());
+        dto.setBid_amount(Double.toString(request.getBidAmount()));
+        dto.setCampaign_amount(Double.toString(request.getCampaignAmount()));
+        dto.setCity(request.getCity());
+        dto.setRadius(Double.toString(request.getRadius()));
+        return dto;
     }
 }
