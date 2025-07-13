a93ee5467badf28f3391bd52de6471557a781640
Margiorno <m.zawiski03@gmail.com>
2025-07-13 16:57:09 +0200

    Api-gateway set and configered to forward requests

--- campaign_service/src/main/java/com/pm/campaign_service/model/Campaign.java
+++ campaign_service/src/main/java/com/pm/campaign_service/model/Campaign.java
@@ -8,9 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-
-
-// TODO maybe separate Campaign template from campaign instance
 @Entity
 public class Campaign {
     @Id
